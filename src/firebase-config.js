3faf5af4173c9e4fc2d889e9a2decb5a07a74084
julia.paulin <julia.paulin@chasacademy.se>
2024-03-26 09:01:07 +0100

    change firebase placement

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,8 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { firebaseConfig } from "./configApi"
 
 // Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD41UN5htNvFOimWb0Jc4ZeD6AZARiCIXA",
+  authDomain: "jobchaser-e474e.firebaseapp.com",
+  projectId: "jobchaser-e474e",
+  storageBucket: "jobchaser-e474e.appspot.com",
+  messagingSenderId: "574262331469",
+  appId: "1:574262331469:web:48e99f54f6b2a1a36fb404",
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
